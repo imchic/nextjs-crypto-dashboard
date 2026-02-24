d10463b923679e11ff1edb1897266f37fe131ebb
im.hyobin <imchic8@gmail.com>
2026-02-24 11:04:21 +0900

    fix: Lottie 로딩 문제 해결 - CSS 애니메이션으로 변경
    문제:
    - lottie-react 패키지 로드 문제
    - 모바일 호환성 이슈
    
    해결책:
    - Lottie 제거
    - 순수 CSS 애니메이션으로 변경
    - shimmer + loadingWidth 조합
    
    개선사항:
    - 더 가볍고 빠른 로딩
    - 모든 기기에서 완벽하게 동작
    - 입체적인 프로그래스바 유지
    
    ⚡ 성능:
    - 번들 사이즈 감소
    - 렌더링 성능 향상
    - 모바일 최적화

--- components/LottieLoadingBar.js
+++ components/LottieLoadingBar.js
@@ -1,16 +1,13 @@
-import Lottie from 'lottie-react';
-import loadingAnimation from '@/public/animations/loading-bar.json';
 import styles from '@/styles/dashboard.module.css';
 
 export default function LottieLoadingBar() {
   return (
     <div className={styles.lottieContainer}>
-      <Lottie 
-        animationData={loadingAnimation}
-        loop={true}
-        autoplay={true}
-        style={{ width: 200, height: 50 }}
-      />
+      <div className={styles.loadingAnimation}>
+        <div className={styles.progressBar}>
+          <div className={styles.progressFill}></div>
+        </div>
+      </div>
       <p>데이터를 불러오는 중입니다...</p>
     </div>
   );
